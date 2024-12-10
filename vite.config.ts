2a22e90a572ec9a942f790b0dabf455fd47296e6
mravu <manaswitha.ravu@gmail.com>
2024-12-09 22:36:16 -0500

    fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),  // Alias to the src folder
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.ts', // Ensure this points to your setup file
-  },
+  }
 });
